41582f81b8c3f1b2beef7d1af16fa0b15ac47ad1
Matthew Josephs <machrome1@gmail.com>
2018-05-01 10:55:36 -0400

    Added lena.jpg and cleaned up main

--- Main.cpp
+++ Main.cpp
@@ -5,19 +5,8 @@ using namespace cv;
 
 int main() {
 	Mat img = imread("lena.jpg");
-	Mat out;
-	Mat kernel = (Mat_<char>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
-
-	//filter2D(img, out, img.depth(), kernel);
-
 	namedWindow("image", WINDOW_NORMAL);
 	imshow("image", img);
-
-	//namedWindow("image_out", WINDOW_NORMAL);
-	//imshow("image", out);
-
 	waitKey(0);
-
-
 	return 0;
 }	
