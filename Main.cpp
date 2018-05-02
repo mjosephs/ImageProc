45acd68d791309fc385f8c1d45b14e5c5a3a4bf1
Renjie Zhou <rzhou10@gmail.com>
2018-05-01 20:17:14 -0400

    Added histo Equalization to main

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include "Sharpen.h"
+#include "Enhance.h"
 
 using namespace std;
 using namespace cv;
@@ -9,13 +10,15 @@ int main(int argc, char** argv) {
 	Mat img = imread(argv[1]);
 	int choice;
 
-	while(1)
+	while (1)
 	{
 		cout << "Choose what to do with image\n";
 		cout << "1. Show image\n";
+		cout << "2. Sharpen image\n";
+		cout << "3. Enhance image using histogram equalization\n";
 
 		//making sure input is an int before continuing
-		while(cout << "Input: " && !(cin >> choice))
+		while (cout << "Input: " && !(cin >> choice))
 		{
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -24,19 +27,21 @@ int main(int argc, char** argv) {
 
 		switch (choice)
 		{
-			case 1 	:	namedWindow("Image Show", WINDOW_AUTOSIZE);
-					 	imshow("Image Show", img);
-						waitKey(0);
-						destroyWindow("Image Show");
-						break;
+		case 1:	namedWindow("Image Show", WINDOW_AUTOSIZE);
+			imshow("Image Show", img);
+			waitKey(0);
+			destroyWindow("Image Show");
+			break;
 
-			case 2:		sharpen(img);
-						break;
+		case 2:		sharpen(img);
+			break;
 
-			default : 	cout << "Invalid input\n";
-						break;
+		case 3:		Enhance(img);
+			break;
+		default: 	cout << "Invalid input\n";
+			break;
 		}
 	}
-	
+
 	return 0;
 }
